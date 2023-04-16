7d1d99872fb20151e0e3505e18dc8ec9be381ea3
Emerson Lima Melo <elimamel@emeal.nttdata.com>
2023-04-16 13:07:58 -0300

    Add the jwt token config

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,16 +1,39 @@
 package com.example.springsecurity.config;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private static final Logger LOGGER = LogManager.getLogger(SecurityConfig.class);
+    private final JwtAuthFilter jwtAuthFilter;
+
+    private static final List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User("teste@gmail.com", "123", Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+            new User("123@gmail.com", "test", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
+    );
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
@@ -21,14 +44,41 @@ public class SecurityConfig {
             http
                     .authorizeRequests().anyRequest().authenticated()
                     .and()
-                    .httpBasic();
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .authenticationProvider(authenticationProvider())
+                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
             return http.build();
         } catch (Exception e) {
             LOGGER.error(METHOD_NAME + ": " + e.getMessage());
             throw new RuntimeException(e);
         }
+    }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    private UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return APPLICATION_USERS
+                        .stream().filter(user -> user.getUsername().equals(username))
+                        .findFirst().orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+            }
+        };
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
